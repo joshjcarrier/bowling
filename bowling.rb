974c420bfc2980f99253287b982d95ff91dbe274
Josh Carrier <joshjcarrier@gmail.com>
2016-10-06 21:14:23 -0700

    Calculate score based on running tally of last frame

--- bowling.rb
+++ bowling.rb
@@ -1,13 +1,32 @@
-class Game
-  def initialize
-    @score = 0
+class Frame
+  def initialize(score: 0)
+    @score = score
+    @rolls = []
   end
 
   def roll(pins)
-    @score += pins
+    @rolls.push(pins)
+
+    # open frame all the time
+    return Frame.new(score: @score + @rolls.reduce(0, :+)) if @rolls.count == 2
+    self
   end
 
   def score
     @score
+  end
+end
+
+class Game
+  def initialize
+    @last_frame = Frame.new
+  end
+
+  def roll(pins)
+    @last_frame = @last_frame.roll(pins)
+  end
+
+  def score
+    @last_frame.score
   end
 end
